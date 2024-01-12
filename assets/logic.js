c8b8adec909c56f8547f8a2d6ac3c12daae518e6
TraceyPen <traceylpender@yahoo.co.uk>
2024-01-12 21:14:52 +0000

    updated files and start button event listener

---
+++ assets/logic.js
@@ -0,0 +1,20 @@
+
+var startButton = document.querySelector('#start')
+
+
+
+
+
+function startGame(){
+    console.log(startButton)
+}
+
+
+
+
+
+
+
+startButton.addEventListener('click', startGame)
+
+
