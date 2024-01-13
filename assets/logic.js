a4633abd949f89a038923542607317dc9c9ed075
TraceyPen <traceylpender@yahoo.co.uk>
2024-01-13 01:08:01 +0000

    startScreen set to not display when start button is pressed

--- assets/logic.js
+++ assets/logic.js
@@ -1,13 +1,19 @@
 
 var startButton = document.querySelector('#start');
 var timeEl = document.querySelector('#time');
-
+var startScreen = document.querySelector('#start-screen')
 
 
 
 
 function startGame(){
     console.log(startButton)
+    startScreen.style.display = 'none';
+    
+timer()
+}
+
+function timer(){
    var secondsLeft = 20;
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -20,11 +26,19 @@ function startGame(){
          
         }
        }, 1000); //1 second at a time
+ }
+
+
 //print first question
-console.log(questions[0], [1]);
+//console.log(questions[0], [1]);
+    
+
+// document.querySelector("#start-screen", display, none);
+// document.querySelector("#questions", text-align, center);
+// document.querySelector("#question-title").textContent = "which is the right answer?";
 
 
-}
+
 
 
 
