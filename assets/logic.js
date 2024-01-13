e93ff54f9966f309fe3c262aeb97c66d15a18831
TraceyPen <traceylpender@yahoo.co.uk>
2024-01-13 18:27:56 +0000

    added event listener to answer choices that responds on click

--- assets/logic.js
+++ assets/logic.js
@@ -33,16 +33,15 @@ function startGame(){
     var li = document.createElement("li");
     li.textContent = questions[0].answerD;
     qchoices.appendChild(li);
-   
-  
-    
-    
-    
-timer()
+
+
+    //call timer function to start the clock
+  timer()
 }
 
 
-//function to start the timer
+
+//function to start the timer --------------------------------------------------------------------
 function timer(){
    var secondsLeft = 20;
     var timerInterval = setInterval(function() {
@@ -57,15 +56,22 @@ function timer(){
         }
        }, 1000); //1 second at a time
  }
+//end of timer code -----------------------------------------------------------------------------------
 
 
-//print first question
-//console.log(questions[0], [1]);
-    
+function checkAnswer(){
+  console.log("click event")
+}
 
-// document.querySelector("#start-screen", display, none);
-// document.querySelector("#questions", text-align, center);
-// document.querySelector("#question-title").textContent = "which is the right answer?";
+
+
+
+
+
+
+
+
+
 
 
 
@@ -77,5 +83,6 @@ function timer(){
 
 
 startButton.addEventListener('click', startGame)
+qchoices.addEventListener('click', checkAnswer)
 
 
